e121db91b34cae295348ef13a232c85752ac6a44
lovbe0210 <lovbe0210@sina.com>
2024-08-12 22:47:05 +0800

    nacos注册成功

--- icharge-auth/src/main/java/com/lovbe/icharge/IchargeAuthApplication.java
+++ icharge-auth/src/main/java/com/lovbe/icharge/IchargeAuthApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class IchargeAuthApplication {
 
     public static void main(String[] args) {
